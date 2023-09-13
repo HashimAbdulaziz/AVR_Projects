358314452eedfd864b7d9d1d81463693a333cb7f
HashimZezo <88584784+HashimAbdulaziz@users.noreply.github.com>
2023-09-13 14:35:45 +0300

    Update App.c
    Added Monitor_Options function, and made display Temp better.

--- Final_Project(smarthome)/Final_Project(smarthome)/App/App.c
+++ Final_Project(smarthome)/Final_Project(smarthome)/App/App.c
@@ -2,7 +2,7 @@
  * App.c
  *
  * Created: 9/7/2023 10:08:39 PM
- *  Author: Marwan
+ *  Author: Hashim Abdulaziz Hashim
  */ 
 #include <util/delay.h>
 
@@ -25,13 +25,15 @@
 
 #include "App.h"
 
-u8 pass[11] = {'9', '5', '1', '7', '5', '3', '6', '4', '8', '2', '0'};
+u8 pass[11] = {'9', '5', '1', '7', '5', '3', '6', '4', '8', '2', '0'};  // Main Password
 u8 pass_copy[11] = {0};
-u8 try = 1;
+u8 try = 1;		// number of tries until system lock
 u16 Temp = 0;
 
+/* Initialize LED*/
 LED_Type LED1 = {LED_PORTC, LED_PIN4, ACTIVE_HIGH};
 
+/* Initialize LM35:  Vref = 5v*/
 LM35_CONFIG lm35 = {ADC_CHANNEL, 5, ADC_RESOLUTION_10_BIT};
 
 
@@ -45,6 +47,8 @@ void Init_peripherals(void)
 		ADC_voidEnable();
 		
 		PMW_voidInit();
+		
+		LED_voidInit(LED1);
 		
 		DIO_voidSetPinDirection(DIO_PORTC, DIO_PIN0, DIO_PIN_OUTPUT);
 		DIO_voidSetPinValue(DIO_PORTC, DIO_PIN0, DIO_PIN_HIGH);
@@ -66,40 +70,37 @@ void System_Locked(void)
 		CLCD_voidSendString("for 2 minutes");
 		_delay_ms(120000);
 	}
-	CLCD_voidClearScreen();
-	CLCD_voidSendString("wrong password,");
-	CLCD_voidSetPosition(2, 1);
-	CLCD_voidSendString("please Try again");
+	if(try != 1)
+	{
+		CLCD_voidClearScreen();
+		CLCD_voidSendString("wrong password,");
+		CLCD_voidSetPosition(2, 1);
+		CLCD_voidSendString("please Try again");
+	}
 }
 
 
-
-void LED_OFF_ON(void)
+void Monitor_Options(void)
 {
 	CLCD_voidClearScreen();
-	CLCD_voidSendString("Welcom Mesho");
+	CLCD_voidSendString("Welcome Mesho");
 	_delay_ms(600);
 	CLCD_voidClearScreen();
 	
-	CLCD_voidSendString("1- Leds ON");
+	CLCD_voidSendString("1-Led ON Temp: ");
 	CLCD_voidSetPosition(2, 1);
-	CLCD_voidSendString("2- Leds OFF");
-	
-	while(1)
-	{
-		u8 val = KYPD_BUTTON_RELEASED;
-		while (val == KYPD_BUTTON_RELEASED || (val != '1' && val != '2' && val != 'c'))
-		{
-			val = KYPD_u8GetPressedKey();
-		}
-		if(val == 'c')
-		break;
-		LED_voidInit(LED1);
-		if(val == '1')
+	CLCD_voidSendString("2-Led OFF");
+}
+
+
+void LED_OFF_ON(void)
+{	
+	u8 val = KYPD_BUTTON_RELEASED;
+	val = KYPD_u8GetPressedKey();
+	if(val == '1')
 		LED_voidOn(LED1);
-		else if(val == '2')
+	else if(val == '2')
 		LED_voidOff(LED1);
-	}
 }
 
 
@@ -108,6 +109,7 @@ u8 Check_Password(void)
 {
 	if(try == 1)
 	{
+		CLCD_voidClearScreen();
 		CLCD_voidSendString("Please Enter");
 		CLCD_voidSetPosition(2, 1);
 		CLCD_voidSendString("Password");
@@ -144,9 +146,7 @@ void Conrol_Motor(void)
 {
 	LM35_u8GetTemp(&lm35, &Temp);
 	
-	
-	CLCD_voidClearScreen();
-	//CLCD_voidSendString("Temp: ");
+	CLCD_voidSetPosition(1, 15);
 	CLCD_voidSendNumber(Temp);
 	if(Temp<=25)
 	{
