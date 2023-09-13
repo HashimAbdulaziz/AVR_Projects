863ad4fcb570bbb1a6351fccd03991b35556688d
HashimZezo <88584784+HashimAbdulaziz@users.noreply.github.com>
2023-09-13 14:36:22 +0300

    Update App.h

--- Final_Project(smarthome)/Final_Project(smarthome)/App/App.h
+++ Final_Project(smarthome)/Final_Project(smarthome)/App/App.h
@@ -2,7 +2,7 @@
  * App.h
  *
  * Created: 9/7/2023 10:08:52 PM
- *  Author: Marwan
+ *  Author: Hashim Abdulaziz Hashim
  */ 
 #include "../03_LIB/STD_TYPES.h"
 
@@ -13,6 +13,7 @@
 void Init_peripherals(void);
 u8 Check_Password(void);
 void System_Locked(void);
+void Monitor_Options(void);
 void LED_OFF_ON(void);
 void Conrol_Motor(void);
 
